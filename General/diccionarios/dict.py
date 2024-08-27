c537defb485c62652fb896a5551a8616f4cf342e
John <jyaxp1@miumg.edu.gt>
2024-08-26 21:46:03 -0600

    Inicio, diccionarios

---
+++ General/diccionarios/dict.py
@@ -0,0 +1,12 @@
+
+# Estructura de un diccionario en Python
+ages = {
+    "Peter": 10,
+    "Isabel": 11,
+    "Anna": 9,
+    "Thomas": 10,
+    "Bob": 10,
+    "Joseph": 11,
+    "Maria": 12,
+    "Gabriel": 10,
+}
