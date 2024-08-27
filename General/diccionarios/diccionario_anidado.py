34314b75b7478b02d119868fa601c444d692db8c
John <jyaxp1@miumg.edu.gt>
2024-08-26 22:28:58 -0600

    add, diccionario anidado

---
+++ General/diccionarios/diccionario_anidado.py
@@ -0,0 +1,18 @@
+# Podemos tener un diccionario anidado, 
+# es decir, un diccionario dentro de otro diccionario
+# Ejemplo:
+estudiantes = {
+    'Peter': {
+        'age': 10, 
+        'address': 'Lisbon'
+    },
+    'Isabel': 
+        {'age': 11, 
+        'address': 'Sesimbra'
+    },
+    'Anna': 
+        {'age': 9, 
+        'address': 'Lisbon'
+    },
+}
+print(estudiantes)
